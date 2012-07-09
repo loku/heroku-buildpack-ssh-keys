15a27f16c53c3a4e883214668e32660cd2943a42
Tim Shadel <tim.shadel@familysearch.org>
2012-07-09 12:15:08 -0600

    Make sure the buildpack detects only deploys with SSH keys
    --Tim Shadel <tim.shadel@familysearch.org>

---
+++ test/detect_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testDetect()
+{
+  mkdir ${BUILD_DIR}/deploy
+  touch ${BUILD_DIR}/deploy/id_rsa
+  
+  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
+  
+  assertEquals 0 ${rtrn}
+  assertEquals "Private SSH Keys" "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+}
+
+testNoDetectMissingSSHKey()
+{
+  mkdir ${BUILD_DIR}/deploy
+
+  capture ${BUILDPACK_HOME}/bin/detect ${BUILD_DIR}
+ 
+  assertEquals 1 ${rtrn}
+  assertEquals "Can't find the ssh key file" "$(cat ${STD_OUT})"
+  assertEquals "" "$(cat ${STD_ERR})"
+}
--- test/simple_test.sh
+++
@@ -1,3 +0,0 @@
-. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
-
-echo ${BUILDPACK_HOME}
